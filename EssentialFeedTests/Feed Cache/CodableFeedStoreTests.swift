5304263df1221870a2f042b9ba85f0b3d0547f60
Dennis Yaremenko <yaremdennis@gmail.com>
2024-10-15 13:48:30 +0300

    Insert delivers error on insertion error

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -81,11 +81,15 @@ class CodableFeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
-        completion(nil)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -166,27 +170,27 @@ class CodableFeedStoreTests: XCTestCase {
         let firstInsertionError = insert((uniqueImageFeed().local, Date()), to: sut)
         XCTAssertNil(firstInsertionError, "Expected to insert cache successfully")
         
-        
         let latestFeed = uniqueImageFeed().local
-        
         let latestTimestamp = Date()
-        
         let latestInsetionError = insert((latestFeed, latestTimestamp), to: sut)
         
         XCTAssertNil(latestInsetionError, "Expected to override cache successfully")
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
     }
+        
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")!
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        let insertionError = insert((feed, timestamp), to: sut)
+        
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
+     
     // MARK: - Helpers
     
     private func makeSUT(
