d3fef13ce5325e2c33d1bee34a8961d8af6c8387
Dennis Yaremenko <yaremdennis@gmail.com>
2024-10-21 08:15:25 +0300

    Refactor code

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -24,16 +24,33 @@ public class URLSessionHTTPClient: HTTPClient {
     
     public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
         
+        // Options # 1
+//        session.dataTask(with: url) { data, response, error in
+//            if let error {
+//                completion(.failure(error))
+//            }
+//            else if let data = data, let response = response as? HTTPURLResponse {
+//                completion(.success((data, response)))
+//            } else {
+//                completion(.failure(UnexpectedValueRepresentation()))
+//            }
+//        }
+//        .resume()
+        
+        // Options # 2
+        
         session.dataTask(with: url) { data, response, error in
-            if let error {
-                completion(.failure(error))
-            }
-            else if let data = data, let response = response as? HTTPURLResponse {
-                completion(.success((data, response)))
-            } else {
-                completion(.failure(UnexpectedValueRepresentation()))
-            }
-        }
-        .resume()
+            completion(Result {
+                if let error = error {
+                    /// Если внутри замыкания была выброшена ошибка с помощью throw, то эта ошибка автоматически ловится и преобразуется в .failure.
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    /// Если замыкание завершилось без ошибок, то результат будет типа .success с возвращённым значением.
+                    return (data, response)
+                } else {
+                    throw UnexpectedValueRepresentation()
+                }
+            })
+        }.resume()
     }
 }
