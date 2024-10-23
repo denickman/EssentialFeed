69b97c761491c5a1ac7fafffe094c0272b97185c
Dennis Yaremenko <yaremdennis@gmail.com>
2024-10-23 19:32:08 +0300

    Feed image view loading indicsator is visible while loading image

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -14,7 +14,9 @@ public protocol FeedImageDataLoaderTask {
 }
 
 public protocol FeedImageDataLoader {
-    func loadImageData(from url: URL) -> FeedImageDataLoaderTask
+    typealias Result = Swift.Result<Data, Error>
+
+         func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
 }
 
 final public class FeedViewController: UITableViewController {
@@ -83,7 +85,10 @@ extension FeedViewController {
         cell.locationLabel.text = cellModel.location
         cell.descriptionLabel.text = cellModel.description
         
-        tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url)
+        cell.feedImageContainer.startShimmering()
+                 tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+                     cell?.feedImageContainer.stopShimmering()
+                 }
         
         return cell
     }
