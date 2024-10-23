a8b8a9e654a8106a7eed0bdcd10548aa207c31b4
Dennis Yaremenko <yaremdennis@gmail.com>
2024-10-23 13:05:53 +0300

    Hide loading indicator on both load error and success

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -40,15 +40,20 @@ final public class FeedViewController: UITableViewController {
         loader?.load { [weak self] result in
             
             // self?.tableModel = (try? result.get()) ?? []
-
-            switch result {
-            case let .success(feed):
+            
+            //            switch result {
+            //            case let .success(feed):
+            //                self?.tableModel = feed
+            //                self?.tableView.reloadData()
+            //            case .failure: break
+            //            }
+            //            self?.refreshControl?.endRefreshing()
+            
+            if let feed = try? result.get() {
                 self?.tableModel = feed
                 self?.tableView.reloadData()
-                self?.refreshControl?.endRefreshing()
-                
-            case .failure: break
             }
+            self?.refreshControl?.endRefreshing()
         }
     }
 }
